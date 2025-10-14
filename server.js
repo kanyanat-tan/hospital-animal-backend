045040a964a8f0ff6eb0c43f5f28bffec4b0448a
kanyanat <kanyanat.saengngoen@gmail.com>
2025-10-14 18:07:47 +0700

    add server.js

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const customerRoutes = require('./router/customerRouter')
+const hospitalRoutes = require('./router/hospitalRouter')
 
 const app = express();
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: './config/config.env'})
+
+app.use(express.json());
+app.use(morgan('dev'))
 
 const port = process.env.PORT || 8080
 
-app.listen(port, () => {
-    console.log("Hello");
+app.use("/api/v1/", customerRoutes);
+app.use("/api/v1/customer", customerRoutes);
+app.use("/api/v1/role", customerRoutes);
 
+app.use("/api/v1/hospital", hospitalRoutes);
+app.use("/api/v1/hospital/:id", hospitalRoutes);
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server will run on ${port}`);
+    
 })
 
